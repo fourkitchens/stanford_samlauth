04965396e01e49bc9e4c08edc2e082de05159f92
pookmish <pookmish@gmail.com>
2024-12-03 09:56:02 -0800

    1.1.0

--- tests/src/Unit/Plugin/Block/SamlLogoutBlockTest.php
+++ tests/src/Unit/Plugin/Block/SamlLogoutBlockTest.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
  * Class SamlLogoutBlockTest
  *
  * @package Drupal\Tests\stanford_samlauth\Unit\Plugin\Block
- * @covers \Drupal\stanford_samlauth\Plugin\Block\SamlLoginBlock
+ * @covers \Drupal\stanford_samlauth\Plugin\Block\SamlLogoutBlock
  */
 class SamlLogoutBlockTest extends UnitTestCase {
 
