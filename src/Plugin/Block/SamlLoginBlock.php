9fa5b6e418474c6711a555e6878fe3d1f8902aed
Dave Hansen-Lange <1127349+dalin-@users.noreply.github.com>
2026-04-13 19:45:40 -0400

    Don't force a redirect back to the homepage
    So that when you log in from the homepage you get the default post-login destination (e.g. dashboard).

--- src/Plugin/Block/SamlLoginBlock.php
+++ src/Plugin/Block/SamlLoginBlock.php
@@ -5,12 +5,14 @@ namespace Drupal\stanford_samlauth\Plugin\Block;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RedirectDestination;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
+use Drupal\path_alias\AliasManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Provides a 'Saml Login Block' block.
@@ -23,11 +25,25 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class SamlLoginBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Current uri the block is displayed on.
+   * RedirectDestination service.
+   *
+   * @var \Drupal\Core\Routing\RedirectDestination
+   */
+  private RedirectDestination $redirectDestination;
+
+  /**
+   * Internal path to the front page.
    *
    * @var string
    */
-  protected $currentUri;
+  private mixed $frontPage;
+
+  /**
+   * PathAliasManager service.
+   *
+   * @var \Drupal\path_alias\AliasManagerInterface
+   */
+  private AliasManagerInterface $pathAliasManager;
 
   /**
    * {@inheritDoc}
@@ -37,7 +53,9 @@ class SamlLoginBlock extends BlockBase implements ContainerFactoryPluginInterfac
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('request_stack')
+      $container->get('redirect.destination'),
+      $container->get('config.factory'),
+      $container->get('path_alias.manager')
     );
   }
 
@@ -50,12 +68,15 @@ class SamlLoginBlock extends BlockBase implements ContainerFactoryPluginInterfac
    *   Block machine name.
    * @param array $plugin_definition
    *   Plugin definition.
-   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
-   *   Current request stack object.
+   * @param \Drupal\Core\Routing\RedirectDestination $redirectDestination
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   * @param \Drupal\path_alias\AliasManagerInterface $pathAliasManager
    */
-  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, RequestStack $requestStack) {
+  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, RedirectDestination $redirectDestination, ConfigFactoryInterface $configFactory, AliasManagerInterface $pathAliasManager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->currentUri = $requestStack->getCurrentRequest()?->getPathInfo();
+    $this->redirectDestination = $redirectDestination;
+    $this->frontPage = $configFactory->get('system.site')->get('page.front');
+    $this->pathAliasManager = $pathAliasManager;
   }
 
   /**
@@ -105,10 +126,32 @@ class SamlLoginBlock extends BlockBase implements ContainerFactoryPluginInterfac
   }
 
   /**
+   * Get the destination of the current request.
+   *
+   * * Returns any destination parameter if present in the current URL.
+   *   E.g. `stanford.edu?destination=/foo/bar`.
+   * * Otherwise the current path.
+   * * Except if that's the homepage, in which case an empty array is returned.
+   *
+   * This ensures that someone clicking the log-in button on the homepage does
+   * not get redirected back to the homepage post-login and instead sees the
+   * standard post-login page (dashboard, etc.).
+   *
+   * @return array
+   */
+  private function getDestination(): array {
+    $front_alias = $this->pathAliasManager->getAliasByPath($this->frontPage);
+    if ($this->redirectDestination->get() === '/' || $this->redirectDestination->get() === $front_alias) {
+      return [];
+    }
+    return $this->redirectDestination->getAsArray();
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function build() {
-    $url = Url::fromRoute('samlauth.saml_controller_login', ['destination' => $this->currentUri]);
+    $url = Url::fromRoute('samlauth.saml_controller_login', $this->getDestination());
     $build = [];
     $build['login'] = [
       '#type' => 'html_tag',
